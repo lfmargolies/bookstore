12dcd7ae809ca41d8b8d4a905d983da52f247e4a
Lee Margolies <lfmargolies@gmail.com>
2014-03-25 22:10:45 -0400

    added params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   def index
-  @available_at = Time.now
-  @books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+ 	@available_at = Time.now
+	@books = Book.all
+  end
+  def show
+  	@book = Book.find(params[:id])
   end
 end
