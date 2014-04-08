08e1ebc4001f9dd868d50d379bb3062998b115fe
Lee Margolies <lfmargolies@gmail.com>
2014-04-08 19:16:35 -0400

    lab 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,15 +15,21 @@ class BooksController < ApplicationController
   end
   def create
   	@book = Book.new(book_params)
-  	@book.save
-  	redirect_to @book
+  	if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
   def edit
     @book = Book.find(params[:id])
   end
   def update
-    @book.update(book_params)
+    if @book.update(book_params)
     redirect_to @book
+    else
+      render :new
+    end
   end
 
 def destroy
